c91f2c5b7368106768a75786447ec16a825c19cd
asonnenschein <adrian.sonnenschein@gmail.com>
2014-07-31 11:26:16 -0700

    refactor contrib form based on rowena's feedback

--- admin.py
+++ admin.py
@@ -2,20 +2,29 @@ from django.contrib import admin
 from azwine.models import Vineyard, Service, Product
 
 class ServiceInline(admin.TabularInline):
-	model = Service
-	extra = 1
+    model = Service
+    extra = 1
 
 class ProductInline(admin.TabularInline):
-	model = Product
-	extra = 1
+    model = Product
+    extra = 1
+    max_num = 3
 
 class VineyardAdmin(admin.ModelAdmin):
-	fieldsets = [
-		(None, {'fields': ['name', 'owner', 'description']}),
-		('Contact', {'fields': ['street', 'county', 'zipcode', 
-			'email', 'phone']}),
-		('GeoLocation', {'fields': ['latitude', 'longitude']}),
-	]
-	inlines = [ServiceInline, ProductInline]
+    fieldsets = [
+        (None, {'fields': ['name', 'owner', 'description',
+            'established', 'website', 'image']}),
+        ('Contact', {'fields': ['street', 'county', 'zipcode',
+            'email', 'phone']}),
+        ('GeoLocation', {'fields': ['latitude', 'longitude']}),
+        ('Hours of Operation', {'fields': ['sunday_open', 'sunday_close',
+            'monday_open', 'monday_close', 'tuesday_open', 'tuesday_close',
+            'wednesday_open', 'wednesday_close', 'thursday_open',
+            'thursday_close', 'friday_open', 'friday_close', 'saturday_open',
+            'saturday_close']}),
+        ('Type of business (select all that apply)', {'fields': ['vineyard',
+            'tasting_room', 'winery']})
+    ]
+    inlines = [ServiceInline, ProductInline]
 
 admin.site.register(Vineyard, VineyardAdmin)
--- models.py
+++ models.py
@@ -1,25 +1,48 @@
 from django.db import models
+from django.conf import settings
 
 class Vineyard(models.Model):
-	name = models.CharField(max_length=500, blank=True)
-	owner = models.CharField(max_length=500, blank=True)
-	street = models.CharField(max_length=1000, blank=True)
-	county = models.CharField(max_length=100, blank=True)
-	zipcode = models.IntegerField()
-	email = models.EmailField(max_length=200, blank=True)
-	phone = models.CharField(max_length=12, blank=True)
-	description = models.TextField(blank=True)
-	latitude = models.DecimalField(max_digits=10, 
-		decimal_places=10, blank=True)
-	longitude = models.DecimalField(max_digits=10, 
-		decimal_places=10, blank=True)
+    name = models.CharField(max_length=500, blank=True)
+    owner = models.CharField(max_length=500, blank=True)
+    street = models.CharField(max_length=1000, blank=True)
+    county = models.CharField(max_length=100, blank=True)
+    zipcode = models.IntegerField()
+    email = models.EmailField(max_length=200, blank=True)
+    phone = models.CharField(max_length=12, blank=True)
+    description = models.TextField(max_length=350, blank=True)
+    established = models.DateField(blank=True)
+    website = models.URLField(max_length=300, blank=True)
+    latitude = models.DecimalField(max_digits=10,
+        decimal_places=10, blank=True)
+    longitude = models.DecimalField(max_digits=10,
+        decimal_places=10, blank=True)
+    sunday_open = models.TimeField(blank=True)
+    sunday_close = models.TimeField(blank=True)
+    monday_open = models.TimeField(blank=True)
+    monday_close = models.TimeField(blank=True)
+    tuesday_open = models.TimeField(blank=True)
+    tuesday_close = models.TimeField(blank=True)
+    wednesday_open = models.TimeField(blank=True)
+    wednesday_close = models.TimeField(blank=True)
+    thursday_open = models.TimeField(blank=True)
+    thursday_close = models.TimeField(blank=True)
+    friday_open = models.TimeField(blank=True)
+    friday_close = models.TimeField(blank=True)
+    saturday_open = models.TimeField(blank=True)
+    saturday_close = models.TimeField(blank=True)
+    vineyard = models.BooleanField(default=False)
+    tasting_room = models.BooleanField(default=False)
+    winery = models.BooleanField(default=False)
+    image = models.ImageField(upload_to=settings.MEDIA_ROOT,
+        height_field=None, width_field=None, max_length=1000,
+        blank=True)
 
 class Service(models.Model):
-	vineyard = models.ForeignKey(Vineyard)
-	service = models.CharField(max_length=500, blank=True)
-	description= models.TextField(blank=True)
+    vineyard_fk = models.ForeignKey(Vineyard)
+    service = models.CharField(max_length=500, blank=True)
+    description= models.TextField(blank=True)
 
 class Product(models.Model):
-	vineyard = models.ForeignKey(Vineyard)
-	product = models.CharField(max_length=500, blank=True)
-	description = models.TextField(blank=True)
+    vineyard_fk = models.ForeignKey(Vineyard)
+    product = models.CharField(max_length=100, blank=True)
+    description = models.TextField(max_length=100, blank=True)
