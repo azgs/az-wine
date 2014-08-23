b310f6e64a5e3cba8017f7a2693fca3f88cd7e82
asonnenschein <adrian.sonnenschein@gmail.com>
2014-08-23 15:57:44 -0700

    combine login and register templates and views to same controller

--- views.py
+++ views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render, render_to_response
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
 from forms import UserForm, UserProfileForm
+from django.contrib.auth import authenticate, login
 
 def homepage(request):
     context = RequestContext(request)
     registered = False
+    username = ''
+    password = ''
 
-    if request.method == "POST":
+
+    if request.POST and request.POST.get('submit') == 'Login':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/admin')
+
+    if request.POST and request.POST.get('submit') == 'Register':
         user_form = UserForm(data=request.POST)
 
         if user_form.is_valid():
@@ -20,7 +34,9 @@ def homepage(request):
             print user_form.errors
     else:
         user_form = UserForm()
-    return render_to_response("azwine/home.html", {
-        "user_form": user_form,
-        "registered": registered
+
+    return render_to_response('azwine/home.html', {
+        'user_form': user_form,
+        'registered': registered,
+        'username': username
     }, context)
