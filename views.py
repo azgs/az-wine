58be7261eac72ded4f30e9a3c231a397d90961fb
asonnenschein <adrian.sonnenschein@gmail.com>
2014-08-23 14:36:39 -0700

    get rid of user_profile contrib

--- views.py
+++ views.py
@@ -4,4 +4,23 @@ from django.template import RequestContext, loader
 from forms import UserForm, UserProfileForm
 
 def homepage(request):
-    pass
+    context = RequestContext(request)
+    registered = False
+
+    if request.method == "POST":
+        user_form = UserForm(data=request.POST)
+
+        if user_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            registered = True
+        else:
+            print user_form.errors
+    else:
+        user_form = UserForm()
+    return render_to_response("azwine/home.html", {
+        "user_form": user_form,
+        "registered": registered
+    }, context)
