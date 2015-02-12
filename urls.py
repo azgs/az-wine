3fbdbf52c34e6043c5c5abe241b2e22355ebc742
Jessica Good <jessica.good@azgs.az.gov>
2015-02-12 18:53:28 +0000

    django migrations has issue with dash in repo name

--- urls.py
+++ urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('az-wine.views',
+urlpatterns = patterns('azwine.views',
     url(r'^$', 'homepage'),
     url(r'^api/rest/vineyards', 'get_all_vineyards'),
 )
